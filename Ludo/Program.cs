9d6a6ec5c85bfe3e5908dd4738225dd0b3aa807f
epicunicornrider <kasper.krumme.madsen@gmail.com>
2018-04-17 15:07:08 +0200

    Startup

---
+++ Ludo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Start a new Ludo Game 
+            // by making an instans of game class
+            Game Ludo = new Game();
+        }
+    }
+}
